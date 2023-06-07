147e823f3e74d2d840ba59e41e65a66290b253d3
Ziraddin <zireddin.ismileoff@gmail.com>
2023-06-07 22:25:57 +0400

    initial commit

---
+++ app/src/main/java/com/zireddinismayilov/ziraddinapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.zireddinismayilov.ziraddinapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
